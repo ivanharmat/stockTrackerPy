6362741bb9a92489ea2a841b4ad22efe1f24841a
Ivan Harmat <ivanharmat@yahoo.com>
2017-10-03 15:59:49 -0400

    enter stock symbol, lookup every 2 minutes, highlihted by colors

--- stockTracker.py
+++ stockTracker.py
@@ -1,4 +1,5 @@
 from colorama import Fore, Back, Style
+import sys
 import time
 import json
 from simplejson import JSONDecodeError
@@ -8,6 +9,10 @@ from urllib.error import HTTPError
 
 print("Stock Tracker by Ivan Harmat")
 print("To cancel press CTRL + C")
+
+stockSymbol = "AAPL"
+if len(sys.argv) > 1 :
+    stockSymbol = sys.argv[1]
 
 lastRefreshedGlobal = ""
 
@@ -20,24 +25,32 @@ def getJSON(url) :
     return jsonObj
 
 def getLastRefreshed(jsonObj) :
-    lastRefreshed = jsonObj["Meta Data"]["3. Last Refreshed"]
+    try :
+        lastRefreshed = jsonObj["Meta Data"]["3. Last Refreshed"]
+    except Exception :
+        return None
     return lastRefreshed
 
 def getLatestStockValue(jsonObj, timeStr) :
-    stockValue = jsonObj["Time Series (1min)"][timeStr]
+    try :
+        stockValue = jsonObj["Time Series (1min)"][timeStr]
+    except Exception :
+        return None 
     return stockValue
 
-def countdown(t):
+def countdown(minutes):
+    seconds = minutes * 60
     try :
-        while t:
-            mins, secs = divmod(t, 60)
-            timeformat = '{:02d}:{:02d}'.format(mins, secs)
-            if t <= 5 :
+        while seconds:
+            mins, secs = divmod(seconds, 60)
+            timeformat = '{:02d}:{:02d}'.format(minutes, secs)
+            if seconds <= 5 :
             	print(Fore.RED + " NEXT LOOKUP IN " + timeformat, end="\r")
             else :
             	print(" NEXT LOOKUP IN " + timeformat, end="\r")
             time.sleep(1)
-            t -= 1
+            seconds -= 1
+            minutes = int(seconds / 60)
         print("                        ", end="\r")
         print(Style.RESET_ALL, end="\r")
         lookupStock()
@@ -48,14 +61,27 @@ def countdown(t):
 
 def lookupStock() :
     global lastRefreshedGlobal
-    jsonObj = getJSON("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=1min&apikey=G49UJ1RC50E64U3R")
+    jsonObj = getJSON("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+stockSymbol+"&interval=1min&apikey=G49UJ1RC50E64U3R")
     lastRefreshed = getLastRefreshed(jsonObj)
-    if lastRefreshed != lastRefreshedGlobal :
-        lastRefreshedGlobal = lastRefreshed
-        stockValueJson = getLatestStockValue(jsonObj, lastRefreshed)
-        print("Latest Stock Value - "+ lastRefreshed + " - "+ Fore.RED  +stockValueJson["1. open"] +" / "+ Fore.BLUE +stockValueJson["4. close"])   
-        print(Style.RESET_ALL, end="\r")
-    countdown(60) 
+    if lastRefreshed != None :
+        if lastRefreshed != lastRefreshedGlobal :
+            lastRefreshedGlobal = lastRefreshed
+            stockValueJson = getLatestStockValue(jsonObj, lastRefreshed)
+            if stockValueJson != None :
+                stockOpen = float(stockValueJson["1. open"])
+                stockClose = float(stockValueJson["4. close"])
+                percent = str(round((stockClose - stockOpen) / 100, 4))
+                if stockOpen > stockClose :
+                    print(stockSymbol + " - Latest Stock Value - "+ lastRefreshed + " - Open : "+ Fore.YELLOW  +str(stockOpen)+Style.RESET_ALL+", Close : "+Fore.CYAN+ str(stockClose)+Fore.RED+" ("+percent+"%)")  
+                elif stockOpen == stockClose:
+                    print(stockSymbol + " - Latest Stock Value - "+ lastRefreshed + " - Open : "+ Fore.YELLOW  +str(stockOpen)+Style.RESET_ALL+", Close : "+Fore.CYAN+ str(stockClose)+Fore.BLUE+" (0%)")
+                else :
+                    print(stockSymbol + " - Latest Stock Value - "+ lastRefreshed + " - Open : "+ Fore.YELLOW  +str(stockOpen)+Style.RESET_ALL+", Close : "+Fore.CYAN+ str(stockClose)+Fore.GREEN+" ("+percent+"%)")
+
+            print(Style.RESET_ALL, end="\r")
+        countdown(2)
+    else :
+        print("Wrong stock symbol - "+stockSymbol) 
 
 lookupStock()
 
